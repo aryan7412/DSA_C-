c9ca2bfcc7770705b604cf869a1b77568102a927
aryan7412 <aryan7412ss@gmail.com>
2024-09-04 00:04:10 +0530

    Sep 4

--- 031.SelectionSort.cpp
+++ 031.SelectionSort.cpp
@@ -1,59 +1,39 @@
 #include<iostream>
 using namespace std;
-
-int search(int arr[], int element, int n)
+void SelectionSort(int arr[],int n)
 {
-    int start = 0;
-    int end = n - 1;
-    int mid;
-
-    while(start <= end)
+    //arr = [4,3,2,1]
+    for(int i=0; i<n-1; i++)
     {
-        mid = start + (end - start) / 2;
-
-        if(arr[mid] == element)
+        int minIndex = i;
+        for(int j=i+1; j<n; j++)
         {
-            return mid;
+            if(arr[j]<arr[minIndex])
+            {
+                minIndex = j;
+            }
         }
-        else if(arr[mid] < element)
-        {
-            start = mid + 1;
-        }
-        else
-        {
-            end = mid - 1;
-        }
+        swap(arr[minIndex],arr[i]);
     }
-
-    return -1;
 }
-
 int main()
 {
-    int arr[100], element, n;
-
+    int arr[100];
+    int n;
     cout << "Enter the size of the array:" << endl;
-    cin >> n;
-
-    cout << "Enter the array elements:" << endl;
-    for(int i = 0; i < n; i++)
+    cin >> n ;
+    cout << "Enter the Array:" << endl;
+    for(int i=0; i<n; i++)
     {
-        cin >> arr[i];
+        cin >> arr[i] ;
     }
+    SelectionSort(arr, n);
 
-    cout << "Enter the element to search:" << endl;
-    cin >> element;
-
-    int index = search(arr, element, n);
-
-    if(index != -1)
-    {
-        cout << "The index of the element is: " << index << endl;
-    }
-    else
-    {
-        cout << "Element not found in the array." << endl;
-    }
+        cout << "Sorted array: " << endl;
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
 
     return 0;
 }
