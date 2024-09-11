304305c315989753fe4fd9abd5890b031a718786
aryan7412 <aryan7412ss@gmail.com>
2024-09-11 23:10:32 +0530

    11sep

--- 035CharArrays.cpp
+++ 035CharArrays.cpp
@@ -1,36 +1,73 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void reverse(char name[20],int n)
-{
+
+void reverse(char name[20], int n) {
     int s = 0;
-    int e = n-1;
-    if(s<e)
-    {
-        swap(name[s++],name[e--]);
+    int e = n - 1;
+    while (s < e) {
+        swap(name[s++], name[e--]);
     }
 }
-int getLength(char name[20])
-{
+
+char toLower(char ch) {
+    if (ch >= 'a' && ch <= 'z') {
+        return ch;
+    } else {
+        char temp = ch - 'A' + 'a';
+        return temp;
+    }
+}
+
+bool isAlphaNumeric(char ch) {
+    return ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9'));
+}
+
+bool checkPalindrome(char name[20], int len) {
+    int start = 0;
+    int end = len - 1;
+    while (start < end) {
+        while (start < end && !isAlphaNumeric(name[start])) {
+            start++;
+        }
+        while (start < end && !isAlphaNumeric(name[end])) {
+            end--;
+        }
+        if (toLower(name[start]) != toLower(name[end])) {
+            return false;
+        } else {
+            start++;
+            end--;
+        }
+    }
+    return true;
+}
+
+int getLength(char name[20]) {
     int count = 0;
-    for(int i=0; name[i]!='\0'; i++)
-    {
+    for (int i = 0; name[i] != '\0'; i++) {
         count++;
     }
     return count;
 }
-int main()
-{
-char name[20];
-cout << "Enter your name:" << endl;
-cin >> name;
 
-cout << "Your Name is:  " << name << endl;
+int main() {
+    char name[20];
+    cout << "Enter your name:" << endl;
+    cin >> name;
+    cout << "Your Name is: " << name << endl;
 
-int len = getLength(name)
-cout << "Total elements: " << len;
+    int len = getLength(name);
+    cout << "Total elements: " << len << endl;
 
-cout << "reverse:" << reverse(name,  )
+    bool p = checkPalindrome(name, len);
+    if (p == false) {
+        cout << "Not a palindrome" << endl;
+    } else {
+        cout << "Is a Palindrome" << endl;
+    }
 
-cout << "Your Name is:  " << name << endl;
-return 0;
+    reverse(name, len);
+    cout << "Reversed Name is: " << name << endl;
+
+    return 0;
 }
