32de98020018012fe8179cee03c8e15e938ae0c0
aryan7412 <aryan7412ss@gmail.com>
2024-09-18 21:51:28 +0530

    18seo

--- 036.Heap.cpp
+++ 036.Heap.cpp
@@ -71,11 +71,11 @@ void heapify(int arr[], int n, int i)
     int left = 2*i;
     int right = 2*i + 1;
 
-    if(left < n  &&  arr[left] > arr[largest])
+    if(left <= n  &&  arr[left] > arr[largest])
     {
         largest = left;
     }
-    if(right < n  &&  arr[right] > arr[largest])
+    if(right <= n  &&  arr[right] > arr[largest])
     {
         largest = right;
     }
@@ -83,6 +83,16 @@ void heapify(int arr[], int n, int i)
     {
         swap(arr[largest],arr[i]);
         heapify(arr,n,i);
+    }
+}
+void heapsort(int arr[],int n)
+{
+    int size = n;
+    while(size > 1)
+    {
+        swap(arr[size],arr[1]);
+        size--;
+        heapify(arr, size, 1);
     }
 }
 int main()
@@ -103,6 +113,13 @@ int main()
     {
         heapify(arr, n, i);
     }
+    for(int i= 1; i<=n; i++)
+    {
+        cout<< arr[i] << ' ';
+    }
+    cout << endl;
+
+    heapsort(arr, n);
     for(int i= 1; i<=n; i++)
     {
         cout<< arr[i] << ' ';
